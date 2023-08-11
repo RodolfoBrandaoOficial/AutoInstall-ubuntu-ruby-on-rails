f704c6944c273b69ae7bdf2fbff7f8d5f67cc4a8
Rodolfo Brandão <31350371+QuanticoBit@users.noreply.github.com>
2023-08-11 13:28:25 -0300

    Update auto-ubuntu-20-04.sh

--- auto-ubuntu-20-04.sh
+++ auto-ubuntu-20-04.sh
@@ -3,13 +3,22 @@
 ## :D
 
 
+# Set the log file path
+LOG_FILE="installation_log.txt"
+
+# Function to log commands and their output
+log_command() {
+    echo "Running: $1" >> "$LOG_FILE"
+    eval "$1" >> "$LOG_FILE" 2>&1
+}
+
 # Install dependencies for compiling Ruby
-yes | sudo apt-get update
-yes | sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev
+log_command "yes | sudo apt-get update"
+log_command "yes | sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev"
 
 # Install ASDF if not already installed
 if [ ! -d "$HOME/.asdf" ]; then
-    git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf"
+    log_command "git clone https://github.com/asdf-vm/asdf.git \"$HOME/.asdf\""
     echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
     echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
     echo 'legacy_version_file = yes' >> ~/.asdfrc
@@ -18,33 +27,34 @@ if [ ! -d "$HOME/.asdf" ]; then
 fi
 
 # Install ASDF plugins
-yes | asdf plugin add ruby
-yes | asdf plugin add nodejs
+log_command "yes | asdf plugin add ruby"
+log_command "yes | asdf plugin add nodejs"
 
 # Install Ruby
-yes | asdf install ruby 3.2.2
-yes | asdf global ruby 3.2.2
-yes | gem update --system
+log_command "yes | asdf install ruby 3.2.2"
+log_command "yes | asdf global ruby 3.2.2"
+log_command "yes | gem update --system"
 
 # Install Node.js and Yarn
-yes | asdf install nodejs 18.16.1
-yes | asdf global nodejs 18.16.1
-yes | npm install -g yarn
+log_command "yes | asdf install nodejs 18.16.1"
+log_command "yes | asdf global nodejs 18.16.1"
+log_command "yes | npm install -g yarn"
 
 # Install Rails
-yes | gem install rails -v 7.0.6
+log_command "yes | gem install rails -v 7.0.6"
 
 # Configure VS Code as the default editor
 echo 'export EDITOR="code --wait"' >> ~/.bashrc
 exec $SHELL
 
 # Display installed versions
-echo "Ruby version:"
-ruby -v
-echo "Node.js version:"
-node -v
-echo "Rails version:"
-rails -v
+log_command "echo \"Ruby version:\""
+log_command "ruby -v"
+log_command "echo \"Node.js version:\""
+log_command "node -v"
+log_command "echo \"Rails version:\""
+log_command "rails -v"
 
-echo "Trust in god!"
-echo "Setup complete!"
+echo "Setup complete!" | tee -a "$LOG_FILE"
+echo "Trust in god!"  | tee -a "$LOG_FILE"
+
